c4908d91ef03464e860fee20c623832a5f7b4ad2
Nicolas Corbalan <outreef@hotmail.com>
2020-09-10 13:05:21 +0200

    commit first

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDWeBouqdOXLqZ7FWXULsgAlroYFr6vBm0",
+  authDomain: "clone-3c259.firebaseapp.com",
+  databaseURL: "https://clone-3c259.firebaseio.com",
+  projectId: "clone-3c259",
+  storageBucket: "clone-3c259.appspot.com",
+  messagingSenderId: "1015433053520",
+  appId: "1:1015433053520:web:ad94a7cb491b3ea7d15855"
+};
