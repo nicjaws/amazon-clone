c64327cf402915a7140b973cde06c32c23792858
Nicolas Corbalan <outreef@hotmail.com>
2020-11-21 09:48:35 +0100

    creating payments

---
+++ src/Payment.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Payment.css'
+
+function Payment() {
+  return (
+    <div className='payment'>
+      <div className='payment__container'>
+        {/* delivery address */}
+        <div className='payment__section'>
+          <div className='payment__title'>
+            
+          </div>
+
+        </div>
+
+        {/* review items */}
+        <div className='review__section'>
+          
+        </div>
+
+        {/* payment method */}
+        <div className='payment__section'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Payment
--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,9 +3,12 @@ import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
 import { getBasketTotal } from './reducer';
+import { useHistory } from 'react-router-dom';
 
 
 function Subtotal() {
+  const history = useHistory();
+  
   const [{basket}, dispatch] = useStateValue();
 
   return (
@@ -29,7 +32,7 @@ function Subtotal() {
         thousanSeparator={true}
         prefix={"$"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
     </div>
   )
 }
